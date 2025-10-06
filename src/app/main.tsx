be7f0a1b34a0ccdae666ad2a432316f0d472914f
Muhammad Umair <muhammadumair523@gmail.com>
2025-10-06 16:34:29 +0500

    minor fixes

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,17 +1,20 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import '@/shared/services/supabase/client'
-import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "@/shared/services/supabase/client";
+import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "@/shared/components/Toaster";
-import { Provider } from 'react-redux'
-import { store } from '@/shared/services/store'
+import { Provider } from "react-redux";
+import { store } from "@/shared/services/store";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-      <Toaster />
-    </BrowserRouter>
-  </Provider>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
